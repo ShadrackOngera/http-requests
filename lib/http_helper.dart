64d71213042e24b2452d48583a45b5a2167a2543
shadrackongera <shadrack.sme@gmail.com>
2023-04-04 06:53:36 +0300

    helper function

---
+++ lib/http_helper.dart
@@ -0,0 +1,24 @@
+import 'package:http/http.dart' as http;
+
+class HttpHelper{
+
+  // Fetching all items
+  Future<List<Map>> fetchItems() async{
+    List<Map> items = [];
+
+    //Get data from the API
+
+    return items;
+  }
+
+
+  //Fetching one item
+  Future<Map> getItem(itemId) async {
+    Map item = {};
+
+    //Get Item from the API
+
+    return item;
+  }
+
+}
